f37fd55ac6dbad71ba087e345ce5b0355eda189f
Ethan.OToole <eotoole005@gmail.com>
2025-04-16 14:39:57 -0400

    Completed Student-Final-Grade

---
+++ Student_Final_Grade/src/main/java/com/pluralsight/FinalGradeApp.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FinalGradeApp {
+    public static void main(String[] args) {
+        System.out.println(finalGrade(85, 11));
+
+    }
+    public static int finalGrade(int exam, int projects){
+        if(exam > 90 || projects > 10) {
+            return 100;
+        } else if (exam > 75 && projects >= 5) {
+            return 90;
+        } else if (exam > 50 && projects >= 2) {
+            return 75;
+        }
+        else {
+            return 0;
+        }
+    }
+}
