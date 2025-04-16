266810b2a7abf04e5f80c21a583c1db3c772e905
Ethan.OToole <eotoole005@gmail.com>
2025-04-16 13:05:51 -0400

    Assertion error fixed. Completed Solution

--- Regexp_Basics_Is_It_A_Digit/src/main/java/com/pluralsight/IsDigitApp.java
+++ Regexp_Basics_Is_It_A_Digit/src/main/java/com/pluralsight/IsDigitApp.java
@@ -4,25 +4,25 @@ package com.pluralsight;
 
 public class IsDigitApp {
     public static boolean isDigit(String s) {
-        s = s.trim();
-        if(s.isEmpty()) {
+        if (s.isEmpty()) {
             return false;
         }
-        else if (!s.isEmpty()){
-            for (int i = 0; i < s.length(); i++) {
-                if(!Character.isDigit(s.charAt(i))){
-                    return false;
-                }
-            }
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
+        try {
+            int check = 0;
+            check = Integer.parseInt(s);
 
+            if (check > 9 || check < 0) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        boolean check = isDigit("14");
+        boolean check = isDigit("1ae45");
         System.out.println(check);
     }
 }
